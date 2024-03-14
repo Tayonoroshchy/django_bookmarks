002b86c7059f64497c7689121d836be443fa1086
tayonoroshchy <rbhbkk-2000@mail.ru>
2024-03-14 17:19:50 +0500

    Adding endless scrolling

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -1,7 +1,7 @@
 const siteUrl = 'https://127.0.0.1:8000/';
 const styleUrl = siteUrl + 'static/css/bookmarklet.css';
-const minWidth = 100;
-const minHeight = 100;
+const minWidth = 250;
+const minHeight = 250;
 
 var head = document.getElementsByTagName('head')[0];
 var link = document.createElement('link');
